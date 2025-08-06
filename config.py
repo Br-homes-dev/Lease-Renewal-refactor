01add0b28560786a3c24a575585fb56b56aa7373
Berry Rock <oklahomefinancing@gmail.com>
2025-08-06 14:31:46 -0500

    populated .env and made more changes to get server.py up and running

--- config.py
+++ config.py
@@ -1,32 +1,23 @@
-import unittest
-from business import get_threshold
-from config import (
-    THRESHOLD_1_LIMIT, THRESHOLD_2_LIMIT, THRESHOLD_3_LIMIT,
-    THRESHOLD_1_VALUE, THRESHOLD_2_VALUE, THRESHOLD_3_VALUE, THRESHOLD_4_VALUE
+import os
+import logging
+
+THRESHOLD_1_LIMIT = 150000
+THRESHOLD_2_LIMIT = 300000
+THRESHOLD_3_LIMIT = 450000
+
+THRESHOLD_1_VALUE = 225.0
+THRESHOLD_2_VALUE = 275.0
+THRESHOLD_3_VALUE = 400.0
+THRESHOLD_4_VALUE = 650.0
+
+ENABLE_LEASE_GENERATION = False
+
+LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO').upper()
+
+logging.basicConfig(
+    level=LOG_LEVEL,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    datefmt='%y-%m-%d %H:%M:%S'
 )
 
-class TestThresholds(unittest.TestCase):
-
-    def test_tier_1_upper_bound(self):
-        self.assertEqual(get_threshold(THRESHOLD_1_LIMIT), THRESHOLD_1_VALUE)
-
-    def test_tier_2_lower_bound(self):
-        self.assertEqual(get_threshold(THRESHOLD_1_LIMIT + 1), THRESHOLD_2_VALUE)
-
-    def test_tier_2_upper_bound(self):
-        self.assertEqual(get_threshold(THRESHOLD_2_LIMIT), THRESHOLD_2_VALUE)
-
-    def test_tier_3_lower_bound(self):
-        self.assertEqual(get_threshold(THRESHOLD_2_LIMIT + 1), THRESHOLD_3_VALUE)
-
-    def test_tier_3_upper_bound(self):
-        self.assertEqual(get_threshold(THRESHOLD_3_LIMIT), THRESHOLD_3_VALUE)
-
-    def test_tier_4_lower_bound(self):
-        self.assertEqual(get_threshold(THRESHOLD_3_LIMIT + 1), THRESHOLD_4_VALUE)
-
-    def test_extremely_high_price(self):
-        self.assertEqual(get_threshold(1_000_000), THRESHOLD_4_VALUE)
-
-if __name__ == '__main__':
-    unittest.main()
+logger = logging.getLogger("lease-renewal")
--- tests/test_thresholds.py
+++ tests/test_thresholds.py
@@ -1,18 +1,32 @@
 import unittest
 from business import get_threshold
+from config import (
+    THRESHOLD_1_LIMIT, THRESHOLD_2_LIMIT, THRESHOLD_3_LIMIT,
+    THRESHOLD_1_VALUE, THRESHOLD_2_VALUE, THRESHOLD_3_VALUE, THRESHOLD_4_VALUE
+)
 
 class TestThresholds(unittest.TestCase):
-    def test_tier_1(self):
-        self.assertEqual(get_threshold(149999), 225.0)
 
-    def test_tier_2(self):
-        self.assertEqual(get_threshold(250000), 300.0)
+    def test_tier_1_upper_bound(self):
+        self.assertEqual(get_threshold(THRESHOLD_1_LIMIT), THRESHOLD_1_VALUE)
 
-    def test_tier_3(self):
-        self.assertEqual(get_threshold(400000), 400.0)
+    def test_tier_2_lower_bound(self):
+        self.assertEqual(get_threshold(THRESHOLD_1_LIMIT + 1), THRESHOLD_2_VALUE)
 
-    def test_tier_4(self):
-        self.assertEqual(get_threshold(500000), 700.0)
+    def test_tier_2_upper_bound(self):
+        self.assertEqual(get_threshold(THRESHOLD_2_LIMIT), THRESHOLD_2_VALUE)
+
+    def test_tier_3_lower_bound(self):
+        self.assertEqual(get_threshold(THRESHOLD_2_LIMIT + 1), THRESHOLD_3_VALUE)
+
+    def test_tier_3_upper_bound(self):
+        self.assertEqual(get_threshold(THRESHOLD_3_LIMIT), THRESHOLD_3_VALUE)
+
+    def test_tier_4_lower_bound(self):
+        self.assertEqual(get_threshold(THRESHOLD_3_LIMIT + 1), THRESHOLD_4_VALUE)
+
+    def test_extremely_high_price(self):
+        self.assertEqual(get_threshold(1_000_000), THRESHOLD_4_VALUE)
 
 if __name__ == '__main__':
     unittest.main()
